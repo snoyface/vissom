583219011cd2a86ef9210454516b86b6d08684c6
Jon MacLeod <j.macleod4@gmail.com>
2016-01-18 17:00:00 -0500

    added interface.io, started work on test for interface.js when page is loaded via express. as of now interface is not working well is Jade templating. Likely dependency issue.

--- vissom2/app.js
+++ vissom2/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var interface = require('interface.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -55,6 +56,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
